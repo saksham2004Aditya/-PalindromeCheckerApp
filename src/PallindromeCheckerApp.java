4799dbc0a7b24f99bcb070dc3bd332bd272d1ede
saksham2004Aditya <sa5048@srmist.edu.in>
2026-02-19 10:44:28 +0530

    feature/UC5

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,13 +1,31 @@
  //PallindromeCheckerApp
- public class PallindromeCheckerApp {
-     public static void main(String[] args) {
-         String hardcoded = "madam";
-         System.out.println("Checking hardcoded word: " + hardcoded);
 
-         if (hardcoded.equals("madam")) {
-             System.out.println(hardcoded + " is a palindrome.");
-         } else {
-             System.out.println(hardcoded + " is not a palindrome.");
+  import java.util.Scanner;
+import java.util.Stack;
+
+     public class PallindromeCheckerApp {
+         public static void main(String[] args) {
+             Scanner scanner = new Scanner(System.in);
+
+             System.out.print("Enter string: ");
+             String input = scanner.nextLine();
+
+             Stack<Character> stack = new Stack<>();
+             for (char c : input.toCharArray()) {
+                 stack.push(c);
+             }
+
+             String reversed = "";
+             while (!stack.isEmpty()) {
+                 reversed += stack.pop();
+             }
+
+             if (input.equalsIgnoreCase(reversed)) {
+                 System.out.println(input + " is a palindrome.");
+             } else {
+                 System.out.println(input + " is not a palindrome.");
+             }
+
+             scanner.close();
          }
      }
- }

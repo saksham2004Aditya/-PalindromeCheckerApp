a2b1c6c31d95367e149bf8a1e41244f4178d9a48
saksham2004Aditya <sa5048@srmist.edu.in>
2026-02-19 10:18:46 +0530

    feature/UC1

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,2 +1,7 @@
 public class PallindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker App");
+        System.out.println("Version: 1.0");
+
+    }
 }
